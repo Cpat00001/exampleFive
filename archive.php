c9f945eebe16a8402b7164e3531ae41f80cb7e29
Pat <p171982@gmail.com>
2021-06-12 18:18:19 +0100

    loops category/loop posts belonging to categories

--- archive.php
+++ archive.php
@@ -4,21 +4,52 @@
     </div>
     <div class="body">
         <div class="body_subgrid_first_row">
-            
-            <?php 
-                if(have_posts()){
-                    while(have_posts()){
-                        the_post();
-                        ?>
-                        <a href="<?php echo get_permalink();?>"><h1><?php echo the_title();?></h1></a>
-                        <?php 
-                            if ( has_post_thumbnail() ) { 
-                                the_post_thumbnail( 'post_image' ); 
-                            }
-                            the_excerpt(); 
-                    }
-                }
+
+        <?php
+        // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+        // loop categories
+        $lup_query = array(
+            'orderby' => 'name',
+            'order' => 'ASC'
+        );
+
+        $lup_result = get_categories($lup_query);
+        // var_dump($lup_result);
+
+        foreach($lup_result as $result){
+            // var_dump($result);
             ?>
+            <h1><?php echo $result->cat_ID; ?></h1><br>
+            <h1 style="color:red;"><?php echo $result->name; ?></h1><br>
+
+            <!-- nazwa kategorii -->
+            <?php
+            $cat_id = $result->cat_ID;
+
+            $lup_posty = array(
+                'post_type' => 'post',
+                'category' => $cat_id
+            );
+            $postlist = get_posts($lup_posty);
+            // var_dump($postlist);
+
+            foreach($postlist as $post){
+                // var_dump($post);
+                ?>
+                <h5 style="color:green;"><?php echo $post->post_title; ?></h5>
+                <p style="color:red;"><?php echo $post->post_date; ?></p><br>
+                <p style="color:orange;"><?php echo $post->post_excerpt; ?></p>
+            <?php             
+            }
+            ?>
+        <?php    
+        }
+
+        // restore $global 
+        wp_reset_postdata();
+        ?>
+
+
         </div>
     </div>
     <div class="footer">
