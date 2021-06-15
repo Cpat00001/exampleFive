b4f99ebc2b53038526d0a3a207d8f38809751507
Pat <p171982@gmail.com>
2021-06-15 19:38:53 +0100

    styling

--- page-blog.php
+++ page-blog.php
@@ -17,10 +17,13 @@
             'posts_per_page' => 3   
         ));
         ?>
-        <!-- <ul> -->
+   
         <?php
         if($show_all_posts->have_posts()){
             while($show_all_posts->have_posts()){
+                ?>
+                <div class="blogpost_card">
+                <?php 
                 $show_all_posts->the_post();
                 $post = get_post();
                 // var_dump($post);
@@ -32,29 +35,43 @@
                         }
                     ?>
                 </div>
-                <!-- <li style="color:black;"> -->
-                <h5><?php the_title(); ?></h5>
-                    <p><?php echo $post->post_date; ?></p>
-                <!-- </li> -->
-                    <p style="color:black;"><?php echo $post->post_excerpt; ?></p>
+                <div class="blogpost_card_right">
+                    <div class="blogpost_title_date">
+                        <h5><?php the_title(); ?></h5>
+                        <p><?php echo $post->post_date; ?></p>
+                    </div>
+                    <div class="blogpost_exerpt">
+                        <p style="color:black;"><?php echo $post->post_excerpt; ?></p>
+                        <div class="blogpost_wiecej">
+                            <a href="<?php echo get_permalink(); ?>"><h3>wiecej...</h3></a>
+                        </div>
+                    </div>
                     
+                </div>
+                
+            <!-- div closing BlogPost card -->
+            </div>
             <?php
             }
             ?>
+            <div class="pagination">
+            <p>Jeszcze więcej interesujących informacji...</p>
             <?php 
             //pagination
                $big = 999999999; // need an unlikely integer
- 
                echo paginate_links( array(
                    'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
                    'format' => '?paged=%#%',
                    'current' => max( 1, get_query_var('paged') ),
                    'total' => $show_all_posts->max_num_pages
                ) );
-            //end of pagination
+               ?>
+            <!-- end of pagination -->
+            </div>
+        <?php
         }
         ?>
-        <!-- </ul> -->
+    
         <?php 
         // restore $global 
         wp_reset_postdata();
--- page.php
+++ page.php
@@ -4,23 +4,30 @@
     </div>
     <div class="body">
         <div class="body_subgrid_first_row">
-            <h1><?php echo the_title(); ?></h1>
-            <?php 
-                if ( has_post_thumbnail() ) { 
-                    the_post_thumbnail( 'post_image' ); 
-                }
-            
-            ?>
-            <?php 
-                if(have_posts()){
-                    while(have_posts()){
-                        the_post();
-                        ?>
-                            <?php the_content(); ?>
-                        <?php
-                    }
-                }
-            ?>
+            <div class="page_body">
+                    <div class="page_title_picture">
+                    <h1><?php echo the_title(); ?></h1>
+                    <?php 
+                        if ( has_post_thumbnail() ) { 
+                            the_post_thumbnail( 'post_image' ); 
+                        }
+                    
+                    ?>
+                    </div>
+                    <!-- page individual blog post content -->
+                    <div class="page_content">
+                    <?php 
+                        if(have_posts()){
+                            while(have_posts()){
+                                the_post();
+                                ?>
+                                    <p><?php the_content(); ?></p>
+                                <?php
+                            }
+                        }
+                    ?>
+                    </div>
+            </div>
         </div>
     </div>
     <div class="footer">
